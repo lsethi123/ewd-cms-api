e547180a3288c78c1404d1651cd4d201fcdfd837
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:08:31 +0300

    updated product model for files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   has_one :line_item
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
