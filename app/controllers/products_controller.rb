7e68faccd52630ef31b9c7e43caaa08abb42fff5
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:08:00 +0300

    updated products with uploading files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :in_stock)
+    params.require(:product).permit(:title, :description, :price, :image, :in_stock)
   end
 
 end
