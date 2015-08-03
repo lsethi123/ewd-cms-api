d026d1ad32ba6529c89356a1cbdfe8778e1855e6
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:08:14 +0300

    updated post model for files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :tag_posts
   has_many :tags, :through => :tag_posts
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
