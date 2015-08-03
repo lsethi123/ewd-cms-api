a2ca0fb893e40adaaa54b51a9d2d5f3b999dc7b9
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:09:24 +0300

    implemented teams module

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,36 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+    render json: @teams
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      render status: 201, json: @team
+    else
+      render status: 404
+    end
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    render json: @team
+  end
+
+  def update
+    @team = Team.find(params[:id]).update(team_params)
+    render json: @team
+  end
+
+  def destroy
+    @team = Team.destroy(params[:id])
+    render json: @team
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
