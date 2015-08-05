1e06588d2a6945ac41089844484bb40572d807f6
Igor Preston <igorpreston@gmail.com>
2015-08-05 15:52:12 +0300

    added todos controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,36 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render status: 201, json: @todo
+    else
+      render status: 404
+    end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  def update
+    @todo = Todo.find(params[:id]).update(todo_params)
+    render json: @todo
+  end
+
+  def destroy
+    @todo = Todo.destroy(params[:id])
+    render json: @todo
+  end
+
+  private
+
+  def todo_params
+    params.require(:task).permit(:title, :user_id, :board_id)
+  end
+end
