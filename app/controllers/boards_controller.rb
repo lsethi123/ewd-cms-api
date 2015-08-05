d5907253d19d71f38b196ed60cd88c0a01ff6afd
Igor Preston <igorpreston@gmail.com>
2015-08-05 15:51:41 +0300

    added boards controller

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,37 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+    end
+    render json: @boards
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      render status: 201, json: @board
+    else
+      render status: 404
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    render json: @board
+  end
+
+  def update
+    @board = Board.find(params[:id]).update(board_params)
+    render json: @board
+  end
+
+  def destroy
+    @board = Board.destroy(params[:id])
+    render json: @board
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :description, :team_id)
+  end
+end
