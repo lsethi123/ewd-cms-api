64c883d8cf16de642c91131d8b0e070661d3e783
Igor Preston <igorpreston@gmail.com>
2015-08-05 15:51:55 +0300

    added checklists controller

---
+++ app/controllers/checklists_controller.rb
@@ -0,0 +1,36 @@
+class ChecklistsController < ApplicationController
+  def index
+    @checklists = Checklist.all
+    render json: @checklists
+  end
+
+  def create
+    @checklist = Checklist.new(checklist_params)
+    if @checklist.save
+      render status: 201, json: @checklist
+    else
+      render status: 404
+    end
+  end
+
+  def show
+    @checklist = Checklist.find(params[:id])
+    render json: @checklist
+  end
+
+  def update
+    @checklist = Checklist.find(params[:id]).update(checklist_params)
+    render json: @checklist
+  end
+
+  def destroy
+    @checklist = Checklist.destroy(params[:id])
+    render json: @checklist
+  end
+
+  private
+
+  def checklist_params
+    params.require(:task).permit(:description, :todo_id)
+  end
+end
