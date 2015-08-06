beeee1c90303df1042b1d8b5eec14a1496366253
Igor Preston <igorpreston@gmail.com>
2015-08-07 00:54:16 +0300

    fixed team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
   has_many :users
-  has_many :companies
-  has_many :boards
+  has_many :companies, :dependent => :destroy
+  has_many :boards, :dependent => :destroy
 end
