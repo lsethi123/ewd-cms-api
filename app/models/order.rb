7bbc8185fe603478472c3790837fc136002af934
Igor Preston <igorpreston@gmail.com>
2015-08-07 00:54:08 +0300

    fixed order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  has_many :line_items
-  has_one :shipping_address
-  has_one :billing_address
+  has_many :line_items, :dependent => :destroy
+  has_one :shipping_address, :dependent => :destroy
+  has_one :billing_address, :dependent => :destroy
 end
