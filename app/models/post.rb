3f004eb9756e9d8ae740b6f79db3927f691ccdd5
Igor Preston <igorpreston@gmail.com>
2015-08-08 04:23:10 +0300

    fixed post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :tag_posts
-  has_many :tags, :through => :tag_posts
+  has_and_belongs_to_many :tags
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
