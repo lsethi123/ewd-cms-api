7d5be9aac2828ef35e8181479cab92e4d7601066
Igor Preston <igorpreston@gmail.com>
2015-08-08 04:23:21 +0300

    fixed image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ActiveRecord::Base
+  has_and_belongs_to_many :pages
+  has_and_belongs_to_many :sliders
   has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment :file, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
